0525eb9b6d7655499ffc52e31da0e83d5df1a91c
toufikhossain11 <toufikhossain546@gmail.com>
2026-07-15 10:36:06 +0600

    atlast Final project

--- src/app/api/auth/[...all]/route.ts
+++
@@ -1,5 +0,0 @@
-// // import { auth } from "@/lib/auth"; // path to our auth file
-// import { auth } from "@/src/lib/auth";
-// import { toNextJsHandler } from "better-auth/next-js";
-
-// export const { POST, GET } = toNextJsHandler(auth);
